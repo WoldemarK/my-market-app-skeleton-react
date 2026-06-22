d602c6141364c18dad666d176a2e07cce30d20f4
WoldemarK <kovtynov.vladimir@gmail.com>
2026-06-22 11:15:27 +0300

    подготовка проекта

---
+++ src/main/java/ru/yandex/mymarketappskeleton/MyMarketAppSkeletonApplication.java
@@ -0,0 +1,13 @@
+package ru.yandex.mymarketappskeleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMarketAppSkeletonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMarketAppSkeletonApplication.class, args);
+    }
+
+}
