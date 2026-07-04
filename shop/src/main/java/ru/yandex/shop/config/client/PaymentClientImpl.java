d30ac9a242eef84370d46f0e48b6690c50609c06
WoldemarK <kovtynov.vladimir@gmail.com>
2026-07-04 18:31:02 +0300

    PaymentClientImpl

--- shop/src/main/java/ru/yandex/shop/config/client/PaymentClientImpl.java
+++ shop/src/main/java/ru/yandex/shop/config/client/PaymentClientImpl.java
@@ -5,12 +5,14 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
+import ru.shop.payment.dto.BalanceResponse;
 import ru.shop.payment.dto.PaymentRequest;
 import ru.shop.payment.dto.PaymentResponse;
+import ru.yandex.shop.client.PaymentClient;
 
 @Service
 @RequiredArgsConstructor
-public class PaymentClientImpl implements PaymentClient{
+public class PaymentClientImpl implements PaymentClient {
     private final WebClient paymentWebClient;
 
     @Override
@@ -21,5 +23,10 @@ public class PaymentClientImpl implements PaymentClient{
                 .bodyValue(request)
                 .retrieve()
                 .bodyToMono(PaymentResponse.class);
+    }
+
+    @Override
+    public Mono<BalanceResponse> getBalance() {
+        return null;
     }
 }
